b98889b7e1ef50891a1f684678aa00c81c29bca8
Pavel Trostianko <rengetsu@mail.ru>
2021-02-09 19:04:13 +0200

    new version

--- src/sensorgraph/SensorGraph.java
+++ src/sensorgraph/SensorGraph.java
@@ -8,6 +8,9 @@ package sensorgraph;
 
 import com.fazecast.jSerialComm.SerialPort;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -18,52 +21,80 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-/**
- *
- * @author pavel
- */
 public class SensorGraph {
-    public static void main(String[] args) {
-        
-        // create and configure the window
-        JFrame window = new JFrame();
-        window.setTitle("Sensor Graph GUI");
-        window.setSize(600, 400);
-        window.setLayout(new BorderLayout());
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // create a drop-down box and connect button, then place them at the top of the window
-        JComboBox<String> portList = new JComboBox<String>();
-        JButton connectButton = new JButton("Connect");
-        JPanel topPanel = new JPanel();
-        
-        // place our combobox and button to the panel
-        topPanel.add(portList);
-        topPanel.add(connectButton);
-        
-        // put that panel in the top region of our window
-        window.add(topPanel, BorderLayout.NORTH);
-        
-        // populate the drop-down box
-        SerialPort[] portNames = SerialPort.getCommPorts();
-        for( int i = 0; i < portNames.length; i++ )
-        {
-            portList.addItem(portNames[i].getSystemPortName());
-        }
-        
-        // Create the line graph
-        XYSeries series = new XYSeries("Light Sensor Readings");
-        XYSeriesCollection dataset = new XYSeriesCollection(series);
-        
-        JFreeChart chart = ChartFactory.createXYLineChart("Light Sensor Readings", "Time (seconds)", "", dataset);
-        
-        series.add(1, 2);
-        window.add(new ChartPanel(chart), BorderLayout.CENTER);
-        
-        // configure the connect button and use another thread to listen for data
-        
-        
-        // show the window
-        window.setVisible(true);
-    }
+	
+	static SerialPort chosenPort;
+	static int x = 0;
+
+	public static void main(String[] args) {
+		
+		// create and configure the window
+		JFrame window = new JFrame();
+		window.setTitle("Sensor Graph GUI");
+		window.setSize(600, 400);
+		window.setLayout(new BorderLayout());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// create a drop-down box and connect button, then place them at the top of the window
+		JComboBox<String> portList = new JComboBox<String>();
+		JButton connectButton = new JButton("Connect");
+		JPanel topPanel = new JPanel();
+		topPanel.add(portList);
+		topPanel.add(connectButton);
+		window.add(topPanel, BorderLayout.NORTH);
+		
+		// populate the drop-down box
+		SerialPort[] portNames = SerialPort.getCommPorts();
+		for(int i = 0; i < portNames.length; i++)
+			portList.addItem(portNames[i].getSystemPortName());
+		
+		// create the line graph
+		XYSeries series = new XYSeries("Light Sensor Readings");
+		XYSeriesCollection dataset = new XYSeriesCollection(series);
+		JFreeChart chart = ChartFactory.createXYLineChart("Light Sensor Readings", "Time (seconds)", "ADC Reading", dataset);
+		window.add(new ChartPanel(chart), BorderLayout.CENTER);
+		
+		// configure the connect button and use another thread to listen for data
+		connectButton.addActionListener(new ActionListener(){
+			@Override public void actionPerformed(ActionEvent arg0) {
+				if(connectButton.getText().equals("Connect")) {
+					// attempt to connect to the serial port
+					chosenPort = SerialPort.getCommPort(portList.getSelectedItem().toString());
+					chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+					if(chosenPort.openPort()) {
+						connectButton.setText("Disconnect");
+						portList.setEnabled(false);
+					}
+					
+					// create a new thread that listens for incoming text and populates the graph
+					Thread thread = new Thread(){
+						@Override public void run() {
+							Scanner scanner = new Scanner(chosenPort.getInputStream());
+							while(scanner.hasNextLine()) {
+								try {
+									String line = scanner.nextLine();
+									int number = Integer.parseInt(line);
+									series.add(x++, 1023 - number);
+									window.repaint();
+								} catch(Exception e) {}
+							}
+							scanner.close();
+						}
+					};
+					thread.start();
+				} else {
+					// disconnect from the serial port
+					chosenPort.closePort();
+					portList.setEnabled(true);
+					connectButton.setText("Connect");
+					series.clear();
+					x = 0;
+				}
+			}
+		});
+		
+		// show the window
+		window.setVisible(true);
+	}
+
 }
